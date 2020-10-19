4a6f56e525ffb41ecda03399039ad8fb3b532399
Vincent Fehr <vincent.fehr1998@gmail.com>
2020-10-19 21:41:50 +0200

    adjusted PanelController
    Added method for button enabling

--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -8,16 +8,11 @@ namespace PixelRainbows.Prototyping
 {
     public class PanelController : MonoBehaviour
     {
-        [SerializeField]
-        protected Transform target;
-        [SerializeField]
-        protected float panelOffset, transitionTime;
-        [SerializeField]
-        protected Button continueButton; 
-        [SerializeField]
-        protected Button returnButton;
-        [SerializeField]
-        protected AnimationCurve transitionCurve;
+        [SerializeField] protected Transform target;
+        [SerializeField] protected float panelOffset, transitionTime;
+        [SerializeField] protected Button continueButton;
+        [SerializeField] protected Button returnButton;
+        [SerializeField] protected AnimationCurve transitionCurve;
 
         int panelCount;
         int currentPanel = 1; //index starts at 1 KEKW
@@ -28,28 +23,20 @@ namespace PixelRainbows.Prototyping
             returnButton.gameObject.SetActive(false);
         }
 
-        private void Update()
-        {
-            if(currentPanel>=2)
-                returnButton.gameObject.SetActive(true);
-            else
-            {
-                returnButton.gameObject.SetActive(false);
-            }
-        }
 
         public void NextPanel()
         {
-            if(currentPanel >= panelCount)
+            if (currentPanel >= panelCount)
                 return;
+
             continueButton.interactable = false;
             returnButton.interactable = false;
             StartCoroutine(GotoNextPanel());
         }
-        
+
         public void LastPanel()
         {
-            if(currentPanel >= panelCount+1)
+            if (currentPanel >= panelCount + 1)
                 return;
             continueButton.interactable = false;
             returnButton.interactable = false;
@@ -60,32 +47,59 @@ namespace PixelRainbows.Prototyping
         {
             Vector2 startPos = target.position;
             Vector2 endPos = new Vector2(startPos.x + panelOffset, startPos.y);
-            for(float t = 0; t < transitionTime; t += Time.deltaTime)
+
+            for (float t = 0; t < transitionTime; t += Time.deltaTime)
             {
-                float ct = transitionCurve.Evaluate(t/transitionTime);
+                float ct = transitionCurve.Evaluate(t / transitionTime);
                 target.position = Vector2.Lerp(startPos, endPos, ct);
                 yield return null;
             }
-            currentPanel ++;
+
+            currentPanel++;
+            ButtonEnabler(true, false);
             continueButton.interactable = true;
             returnButton.interactable = true;
-            target.GetChild(currentPanel-1).gameObject.BroadcastMessage("WakeUp", SendMessageOptions.DontRequireReceiver);
+            target.GetChild(currentPanel - 1).gameObject
+                .BroadcastMessage("WakeUp", SendMessageOptions.DontRequireReceiver);
         }
 
         IEnumerator GoBackOnePanel()
         {
             Vector2 startPos = target.position;
             Vector2 endPos = new Vector2(startPos.x - panelOffset, startPos.y);
-            for(float t = 0; t < transitionTime; t += Time.deltaTime)
+            for (float t = 0; t < transitionTime; t += Time.deltaTime)
             {
-                float ct = transitionCurve.Evaluate(t/transitionTime);
+                float ct = transitionCurve.Evaluate(t / transitionTime);
                 target.position = Vector2.Lerp(startPos, endPos, ct);
                 yield return null;
             }
-            currentPanel --;
+
+            currentPanel--;
+            ButtonEnabler(false, true);
             continueButton.interactable = true;
             returnButton.interactable = true;
         }
 
+        private void ButtonEnabler(bool next, bool back)
+        {
+            if (next == true)
+            {
+                if (currentPanel >= 2)
+                    returnButton.gameObject.SetActive(true);
+
+                if (currentPanel == panelCount)
+                    continueButton.gameObject.SetActive((false));
+
+            }
+
+            if (back == true)
+            {
+                if (currentPanel == 1)
+                    returnButton.gameObject.SetActive(false);
+
+                if (currentPanel <= panelCount)
+                    continueButton.gameObject.SetActive((true));
+            }
+        }
     }
 }
