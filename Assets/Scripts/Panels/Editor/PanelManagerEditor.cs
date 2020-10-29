c26f62768dd1ff564bc5ad1e78ada798ebc6a34f
Toreole <toreole.official@gmail.com>
2020-10-29 13:26:14 +0100

    Started Editor, testing auto-scaling panels

--- Assets/Scripts/Panels/Editor/PanelManagerEditor.cs
+++ Assets/Scripts/Panels/Editor/PanelManagerEditor.cs
@@ -9,6 +9,47 @@ namespace PixelRainbows.Editing
     [CustomEditor(typeof(PanelManager))]
     public class PanelManagerEditor : Editor
     {
-        
+        private PanelManager manager;
+
+        SerializedProperty cameraProperty;
+        SerializedProperty panelList;
+        SerializedProperty panelPrefabProperty;
+        bool foldout = false;
+
+        private void OnEnable() 
+        {
+            manager = target as PanelManager;
+            cameraProperty = serializedObject.FindProperty("camera");
+            panelList = serializedObject.FindProperty("panels");
+            panelPrefabProperty = serializedObject.FindProperty("blankPanelPrefab");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(panelPrefabProperty);
+            EditorGUILayout.PropertyField(cameraProperty);
+            //make sure the camera is assigned before we do anything else
+            if(!cameraProperty.objectReferenceValue || !panelPrefabProperty.objectReferenceValue)
+            {
+                EditorGUILayout.HelpBox("Camera and Prefab need to be assigned in order to edit.", MessageType.Warning);
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+            EditorGUILayout.Separator();
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                EditorGUI.indentLevel++;
+            foldout = EditorGUILayout.Foldout(foldout, "Current Panel (index)");
+            if(foldout)
+            {
+                EditorGUILayout.LabelField("This is content");
+                EditorGUILayout.LabelField("This is content");
+                EditorGUILayout.LabelField("This is content");
+                EditorGUILayout.LabelField("This is content");
+            }
+                EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+            //apply modified properties at the very end.
+            serializedObject.ApplyModifiedProperties();
+        }
     }
 }
--- Assets/Scripts/Panels/PanelManager.cs
+++ Assets/Scripts/Panels/PanelManager.cs
@@ -10,6 +10,7 @@ namespace PixelRainbows.Panels
     ///</summary>
     public class PanelManager : MonoBehaviour
     {
+#region SerializedSettings
         [SerializeField]
         private GameObject blankPanelPrefab;
 
@@ -18,6 +19,17 @@ namespace PixelRainbows.Panels
         
         [SerializeField]
         private new Camera camera;
+
+        [SerializeField]
+        Transform testTarget;
+#endregion
+
+#region Runtime_Vars
+        private Vector2 cameraBounds;
+
+        //The index of the currently selected panel
+        int selectedPanel;
+#endregion
 
 #if UNITY_EDITOR
         //local static instance for the panel manager, just in case its needed.
@@ -28,22 +40,52 @@ namespace PixelRainbows.Panels
         //Does a PanelManager exist in this scene?
         public static bool Exists => Instance;
 
-        //The index of the currently selected panel
-        int selectedPanel;
-
         //Draw gizmos for all the panels in the collection.
         private void OnDrawGizmos() 
         {
+            if(!camera)
+                return;
             //figure out the "real" size of the camera with the current screen bounds.
-            float screenRatio = (float)Screen.width / (float)Screen.height;
-            float height = camera.orthographicSize;
-            float width = screenRatio * height;
+            var bounds = GetCameraBounds();
 
             //draw the camera bounds around each panel so it's easy to see whether it stays within the bounds.
             foreach(var p in panels)
-                Gizmos.DrawWireCube(p.transform.position, new Vector3(width, height));
+                Gizmos.DrawWireCube(p.transform.position, new Vector3(bounds.x, bounds.y));
         }
 #endif
+
+        //Initialize PanelManager for runtime use. Prepare the panels.
+        private void Start() 
+        {
+            cameraBounds = GetCameraBounds();
+        }
+
+        ///<summary>Get the camera's bounds defined by width and height in worldspace. (orthographic camera)</summary>
+        private Vector2 GetCameraBounds()
+        {
+            float screenRatio = (float)Screen.width / (float)Screen.height;
+            float height = camera.orthographicSize;
+            float width = screenRatio * height;
+            return new Vector2(width, height);
+        }
+
+        void Update() => 
+            SetupPanel(testTarget);
+
+        //Just testing something here. (Making sure the object is on-screen in full)
+        //!For some stupid reason this flickers between two results when run in Update.
+        private void SetupPanel(Transform target)
+        {
+            cameraBounds = GetCameraBounds();
+            var sprite = target.GetComponentInChildren<SpriteRenderer>(); //move main renderer component to data type?
+            var spriteSize = sprite.bounds.extents; //width and height of the sprite in the scene.
+            //Debug.DrawLine(sprite.bounds.min, sprite.bounds.max, Color.red, 5);
+            //auto-scale the sprite.
+            float scale = cameraBounds.x/spriteSize.x;
+            target.localScale *= scale;
+            if(target.localScale.y > 1) //dont scale up beyond the default.
+                target.localScale = Vector3.one;
+        }
 
     }
 }
