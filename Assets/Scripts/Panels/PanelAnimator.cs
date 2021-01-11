626a6468cb862728950b0234b74abb09be307dcc
Toreole <toreole.official@gmail.com>
2021-01-11 13:08:06 +0100

    Super basic PanelAnimator component
    Add this to your panels that you want animated when they are on-screen.
    No additional effort required in the animator, this just disables it at the start of the game, and enables it once the panel is fully visible

---
+++ Assets/Scripts/Panels/PanelAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelRainbows
+{
+    public class PanelAnimator : Minigame.MinigameBaseClass
+    {
+        Animator anim;
+        private void Awake() 
+        {
+            anim = GetComponent<Animator>(); //just double check    
+            anim.enabled = false;
+        }
+        public override void CancelMinigame()
+        {
+            //this isnt really a minigame so yeah.
+        }
+
+        public override void WakeUp()
+        {
+            anim.enabled = true;
+            IsDone = true;
+        }
+    }
+}
