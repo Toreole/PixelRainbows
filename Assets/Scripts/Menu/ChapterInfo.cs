2c59e7d8aa13c0dec141e046d814d0fd9019c6dc
Toreole <toreole.official@gmail.com>
2021-02-09 17:49:24 +0100

    Chapter Selection V2

---
+++ Assets/Scripts/Menu/ChapterInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+namespace PixelRainbows
+{
+    public class ChapterInfo : MonoBehaviour, IPointerEnterHandler
+    {
+        [SerializeField]
+        protected GameObject info;
+        [SerializeField]
+        protected GameObject[] others;
+
+        private UnityEngine.UI.Button button;
+
+        private void Start() 
+        {
+            button = GetComponent<UnityEngine.UI.Button>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if(!button.interactable)
+                return;
+            info.SetActive(true);
+            for(int i = 0; i < others.Length; i++)
+                others[i].SetActive(false);
+        }
+
+    }
+}
